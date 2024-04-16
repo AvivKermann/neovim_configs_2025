54baa48d79e004d8e8e3dea991fe8287a9ab3657
AvivKermann <avivkermann@gmail.com>
2024-04-16 18:11:42 +0300

    telescope now finds hidden files

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,4 +1,12 @@
 local builtin = require('telescope.builtin')
+require('telescope').setup({
+    pickers = {
+        find_files = {
+            hidden = true
+        },
+        }
+    })
+
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
