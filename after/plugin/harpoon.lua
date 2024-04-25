5f293e4245ec51855891027044a283e1ffdbcfc1
AvivKermann <avivkermann@gmail.com>
2024-04-25 11:09:05 +0300

    some changes to harpoon config.  Please enter the commit message for your changes. Lines starting

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -1,6 +1,11 @@
 local harpoon = require("harpoon")
 
-harpoon:setup()
+harpoon:setup({
+    global_settings = {
+        save_on_change = true
+    }
+})
+
 vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
 vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 vim.keymap.set("n", "<leader>r", function() harpoon:list():remove() end)
