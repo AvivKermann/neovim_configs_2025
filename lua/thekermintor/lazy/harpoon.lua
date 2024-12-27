b3b520343a663518dc2480e753eb12725ac37551
AvivKermann <avivkermann@gmail.com>
2024-12-27 16:05:50 +0200

    more formatting and changes to harpoon keybindings

--- lua/thekermintor/lazy/harpoon.lua
+++ lua/thekermintor/lazy/harpoon.lua
@@ -6,11 +6,10 @@ return {
 	},
 	config = true,
 	keys = {
-		{ "<leader>a", "<cmd>lua require('harpoon.mark').add_file()<cr>", desc = "Mark file with harpoon" },
-		{ "<leader>r", "<cmd>lua require('harpoon.mark').remove_file()<cr>", desc = "Mark file with harpoon" },
-        { "<C-e>", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", desc = "Show harpoon marks" },
+		{ "<leader>ha", "<cmd>lua require('harpoon.mark').add_file()<cr>", desc = "Mark file with harpoon" },
+		{ "<leader>hr", "<cmd>lua require('harpoon.mark').remove_file()<cr>", desc = "Mark file with harpoon" },
+		{ "<C-e>", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", desc = "Show harpoon marks" },
 		{ "<C-n>", "<cmd>lua require('harpoon.ui').nav_next()<cr>", desc = "Go to next harpoon mark" },
 		{ "<C-p", "<cmd>lua require('harpoon.ui').nav_prev()<cr>", desc = "Go to previous harpoon mark" },
 	},
 }
-
--- lua/thekermintor/lazy/lsp.lua
+++ lua/thekermintor/lazy/lsp.lua
@@ -1,65 +1,64 @@
 --- Which ever servers will be in this table get installed.
 local lsp_servers = {
-    "pyright",
-    "lua_ls",
+	"pyright",
+	"lua_ls",
 }
 return {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-        "williamboman/mason.nvim",
-        "williamboman/mason-lspconfig.nvim",
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
-        "hrsh7th/cmp-cmdline",
-        "hrsh7th/nvim-cmp",
-        "L3MON4D3/LuaSnip",
-        "saadparwaiz1/cmp_luasnip",
-        "rafamadriz/friendly-snippets",
-        "folke/lazydev.nvim",
-    },
-    config = function()
-        local cmp = require("cmp")
-        local cmp_lsp = require("cmp_nvim_lsp")
-        local capabilities = vim.tbl_deep_extend(
-            "force",
-            {},
-            vim.lsp.protocol.make_client_capabilities(),
-            cmp_lsp.default_capabilities()
-        )
+	"neovim/nvim-lspconfig",
+	dependencies = {
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-cmdline",
+		"hrsh7th/nvim-cmp",
+		"L3MON4D3/LuaSnip",
+		"saadparwaiz1/cmp_luasnip",
+		"rafamadriz/friendly-snippets",
+		"folke/lazydev.nvim",
+	},
+	config = function()
+		local cmp = require("cmp")
+		local cmp_lsp = require("cmp_nvim_lsp")
+		local capabilities = vim.tbl_deep_extend(
+			"force",
+			{},
+			vim.lsp.protocol.make_client_capabilities(),
+			cmp_lsp.default_capabilities()
+		)
 
-        require("mason").setup()
-        require("mason-lspconfig").setup({
-            ensure_installed = lsp_servers,
-            automatic_installation = true,
-            handlers = {
-                function(server)
-                    require("lspconfig")[server].setup({
-                        capabilities = capabilities,
-                    })
-            end,
-        }
-
-        })
-        local cmp_select = { behavior = cmp.SelectBehavior.Select }
-        cmp.setup({
-            snippet = {
-                expand = function(args)
-                    require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-                end,
-            },
-            mapping = cmp.mapping.preset.insert({
-                ['<C-k>'] = cmp.mapping.select_prev_item(cmp_select),
-                ['<C-j>'] = cmp.mapping.select_next_item(cmp_select),
-                ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-            }),
-            sources = cmp.config.sources({
-                { name = 'nvim_lsp' },
-                { name = 'luasnip' },
-            }, {
-                { name = 'buffer' },
-            })
-        })
-        require("luasnip.loaders.from_vscode").lazy_load()
-    end,
+		require("mason").setup()
+		require("mason-lspconfig").setup({
+			ensure_installed = lsp_servers,
+			automatic_installation = true,
+			handlers = {
+				function(server)
+					require("lspconfig")[server].setup({
+						capabilities = capabilities,
+					})
+				end,
+			},
+		})
+		local cmp_select = { behavior = cmp.SelectBehavior.Select }
+		cmp.setup({
+			snippet = {
+				expand = function(args)
+					require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+				end,
+			},
+			mapping = cmp.mapping.preset.insert({
+				["<C-k>"] = cmp.mapping.select_prev_item(cmp_select),
+				["<C-j>"] = cmp.mapping.select_next_item(cmp_select),
+				["<C-y>"] = cmp.mapping.confirm({ select = true }),
+			}),
+			sources = cmp.config.sources({
+				{ name = "nvim_lsp" },
+				{ name = "luasnip" },
+			}, {
+				{ name = "buffer" },
+			}),
+		})
+		require("luasnip.loaders.from_vscode").lazy_load()
+	end,
 }
