089c24fdc0fd9b2d457993c61a272495de42e98b
AvivKermann <avivkermann@gmail.com>
2024-12-31 15:59:41 +0200

    moved from nvim-cmp to blink.cmp, seems to be faster and prittier

---
+++ lua/thekermintor/lazy/completion.lua
@@ -0,0 +1,44 @@
+return {
+  'saghen/blink.cmp',
+  dependencies = {
+      'rafamadriz/friendly-snippets',
+      "echasnovski/mini.icons",
+  },
+  version = '*',
+  opts = {
+    keymap = { preset = 'default',
+        ['<C-y>'] = { 'select_and_accept' },
+        ['<C-j>'] = { 'select_next', 'fallback' },
+        ['<C-k>'] = { 'select_prev', 'fallback' },
+    },
+    completion = {
+        menu = {
+            draw = {
+                components = {
+                    kind_icon = {
+                        ellipsis = false,
+                        text = function (ctx)
+                            local kind_icon, _, _ = require("mini.icons").get("lsp", ctx.kind)
+                            return kind_icon
+                        end,
+                        highlight = function (ctx)
+                            local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
+                            return hl
+                        end
+                    }
+                },
+                columns = { { "label", "label_description", gap = 1 }, {"kind", "kind_icon", gap = 1}},
+            }
+        }
+    },
+    appearance = {
+      use_nvim_cmp_as_default = true,
+      nerd_font_variant = 'mono'
+    },
+    signature = { enabled = true },
+    sources = {
+      default = { 'lsp', 'path', 'snippets', 'buffer' },
+    },
+  },
+  opts_extend = { "sources.default" }
+}
--- lua/thekermintor/lazy/lsp.lua
+++ lua/thekermintor/lazy/lsp.lua
@@ -8,29 +8,14 @@ return {
 	dependencies = {
 		"williamboman/mason.nvim",
 		"williamboman/mason-lspconfig.nvim",
-		"hrsh7th/cmp-nvim-lsp",
-		"hrsh7th/cmp-buffer",
-		"hrsh7th/cmp-path",
-		"hrsh7th/cmp-cmdline",
-		"hrsh7th/nvim-cmp",
 		"L3MON4D3/LuaSnip",
-		"saadparwaiz1/cmp_luasnip",
 		"rafamadriz/friendly-snippets",
 		"folke/lazydev.nvim",
-        "onsails/lspkind.nvim",
+        'saghen/blink.cmp',
+
 	},
 	config = function()
-        local lspkind = require("lspkind")
-        lspkind.init()
-		local cmp = require("cmp")
-		local cmp_lsp = require("cmp_nvim_lsp")
-		local capabilities = vim.tbl_deep_extend(
-			"force",
-			{},
-			vim.lsp.protocol.make_client_capabilities(),
-			cmp_lsp.default_capabilities()
-		)
-
+        local capabilities = require("blink.cmp").get_lsp_capabilities()
 		require("mason").setup()
 		require("mason-lspconfig").setup({
 			ensure_installed = lsp_servers,
@@ -42,36 +27,6 @@ return {
 					})
 				end,
 			},
-		})
-		local cmp_select = { behavior = cmp.SelectBehavior.Select }
-		cmp.setup({
-            formatting = {
-                format = lspkind.cmp_format({
-                    mode = "symbol",
-                    maxwidth = {
-                        menu = 50,
-                        abbr = 50,
-                    },
-
-                }),
-            },
-
-			snippet = {
-				expand = function(args)
-					require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
-				end,
-			},
-			mapping = cmp.mapping.preset.insert({
-				["<C-k>"] = cmp.mapping.select_prev_item(cmp_select),
-				["<C-j>"] = cmp.mapping.select_next_item(cmp_select),
-				["<C-y>"] = cmp.mapping.confirm({ select = true }),
-			}),
-			sources = cmp.config.sources({
-				{ name = "nvim_lsp" },
-				{ name = "luasnip" },
-			}, {
-				{ name = "buffer" },
-			}),
 		})
 		require("luasnip.loaders.from_vscode").lazy_load()
 	end,
