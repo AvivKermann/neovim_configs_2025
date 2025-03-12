a3e5dd0ac74f97990d03acb081b14cdc546399c0
AvivKermann <avivkermann@gmail.com>
2025-03-12 09:45:42 +0200

    changes to loading

--- lua/thekermintor/set.lua
+++ lua/thekermintor/set.lua
@@ -48,3 +48,31 @@ vim.diagnostic.config({
         focusable = false,
     },
 })
+
+
+vim.cmd([[
+    augroup CustomNotifyHighlights
+        autocmd!
+        autocmd ColorScheme * highlight NotifyERRORBorder guifg=#ebbcba
+        autocmd ColorScheme * highlight NotifyWARNBorder guifg=#ebbcba
+        autocmd ColorScheme * highlight NotifyINFOBorder guifg=#ebbcba
+        autocmd ColorScheme * highlight NotifyDEBUGBorder guifg=#ebbcba
+        autocmd ColorScheme * highlight NotifyTRACEBorder guifg=#ebbcba
+        autocmd ColorScheme * highlight NotifyERRORIcon guifg=#ebbcba
+        autocmd ColorScheme * highlight NotifyWARNIcon guifg=#ebbcba
+        autocmd ColorScheme * highlight NotifyINFOIcon guifg=#ebbcba
+        autocmd ColorScheme * highlight NotifyDEBUGIcon guifg=#ebbcba
+        autocmd ColorScheme * highlight NotifyTRACEIcon guifg=#ebbcba
+        autocmd ColorScheme * highlight NotifyERRORTitle guifg=#ebbcba
+        autocmd ColorScheme * highlight NotifyWARNTitle guifg=#ebbcba
+        autocmd ColorScheme * highlight NotifyINFOTitle guifg=#ebbcba
+        autocmd ColorScheme * highlight NotifyDEBUGTitle guifg=#ebbcba
+        autocmd ColorScheme * highlight NotifyTRACETitle guifg=#ebbcba
+        autocmd ColorScheme * highlight link NotifyERRORBody Normal
+        autocmd ColorScheme * highlight link NotifyWARNBody Normal
+        autocmd ColorScheme * highlight link NotifyINFOBody Normal
+        autocmd ColorScheme * highlight link NotifyDEBUGBody Normal
+        autocmd ColorScheme * highlight link NotifyTRACEBody Normal
+    augroup END
+    ]])
+
